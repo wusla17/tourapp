c4ec9722e79cd704a2e718beb990816af0c4bb27
Mohammed Arsh <wuslateam@gmail.com>
2023-09-16 23:20:42 +0530

    3 59 finished complete with restof the video

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainNav from "./components/Nav/MainNav";
 import Places from "./components/Screens/Places";
-import Place from "./components/Screens/Place";
+// import Place from "./components/Screens/Place";
 
 function App() {
   return (
     <>
       <Router>
-        <MainNav />
-        <Routes></Routes>
+        <Places />
       </Router>
     </>
   );
